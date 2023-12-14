e0013fee316a0d7251abccdd3ec135717e6ba500
Simeon Markov <simeonmarkov777@gmail.com>
2023-12-14 09:48:57 +0200

    Ex02

---
+++ Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Abstraction_And_Interfaces
+{
+    class Cat : Animal
+    {
+        public Cat(string name, int age) : base(name, age) { }
+
+        public override string  MakeNoise()
+        {
+            return "Meow! " + base.MakeNoise();
+        }
+
+        public override string MakeTrick()
+        {
+            return "No trick for you! I'm too lazy!";
+        }
+    }
+}
