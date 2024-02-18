1e9b6adcde10a6c3c0326d93e1d6237f0ee36139
Simeon Markov <simeonmarkov777@gmail.com>
2024-02-18 20:45:59 +0200

    new exercises

---
+++ Reflections/Theif/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Theif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Spy spy = new Spy();
+            //string result = spy.StealFieldInfo("Theif.Hacker", "username", "password");
+            //Console.WriteLine(result);
+
+            //Spy spy = new Spy();
+            //string result = spy.AnalyzeAccessModifiers("Theif.Hacker");
+            //Console.WriteLine(result);
+
+            //Spy spy = new Spy();
+            //string result = spy.RevealPrivateMethods("Theif.Hacker");
+            //Console.WriteLine(result);
+
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Theif.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
