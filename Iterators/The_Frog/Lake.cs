bcbea339dcb83893f9239a1acdc9c259cd77cadb
Simeon Markov <simeonmarkov777@gmail.com>
2024-02-18 20:24:24 +0200

    New exercise

---
+++ Iterators/The_Frog/Lake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frog
+{
+    class Lake : IEnumerable<int>
+    {
+        private List<int> stoneNumbers;
+
+        public Lake(List<int> stoneNumbers)
+        {
+            this.stoneNumbers = new List<int>(stoneNumbers);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i <= stoneNumbers.Count - 1; i++)
+            {
+
+                if (stoneNumbers[i] % 2 == 1)
+                {
+        
+                    yield return stoneNumbers[i];
+                }
+            }
+        }
+
+        public IEnumerable<int> Enumerator()
+        {
+            for (int i = stoneNumbers.Count - 1; i >= 0; i--)
+            {
+                if (stoneNumbers[i] % 2 == 0)
+                {
+                    stoneNumbers.Sort();
+                    yield return stoneNumbers[i];
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators/The_Frog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Frog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> stoneNumbers = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Lake lake = new Lake(stoneNumbers);
+
+            foreach (int stone in lake)
+            {
+
+                Console.Write($"{stone} ");
+            }
+            foreach (int stone in lake.Enumerator())
+            {
+                Console.Write($"{stone} "); 
+            }
+        }
+    }
+}
